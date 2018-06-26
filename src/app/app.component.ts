a41470ffec174bdc9a80d3cfa60d11830b01044f
unknown <vinayakkumardadhich@gmail.com>
2018-06-26 16:41:16 +0530

    first page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // url="https://upload.wikimedia.org/wikipedia/commons/3/36/Hopetoun_falls.jpg"
+  name="";
+  a=0;
+  number=0;
+  priority=""
+  date=""
+   numbers:any=[]
+    add()
+    {
+      var obj={
+        status:false,name:this.name,priority:this.priority,date: new Date()
+      }
+      this.numbers.push(obj);
+      console.log(this.numbers)
+      this.name="";
+
+    
+    }
+    delete(i){
+      this.numbers.splice(i,1);
+    }
+    done(i){
+      this.numbers[i].status=!this.numbers[i].status;
+      console.log(this.numbers[i].status);
+        if(this.numbers[i].status==true){
+           this.a= this.a-1;
+            
+        }
+        
+          else(this.numbers[i].status==false)
+          {
+           this.a= this.a+1;
+        }
+
+
+    }
+    count1(){
+      var c=0;
+      for(var i=0;i<this.numbers.length;i++){
+              c=i;
+              
+      }
+      return c;
+    }
+    pending(){
+      var count=0;
+      for(var i=0;i<this.numbers.length;i++){
+        if(!this.numbers.status){
+          count++
+        }
+      }
+      return count;
+    }
+   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import{FormsModule}from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule
-  ],
+
+    BrowserModule,
+    FormsModule
+  ]
+  ,
   providers: [],
   bootstrap: [AppComponent]
 })
